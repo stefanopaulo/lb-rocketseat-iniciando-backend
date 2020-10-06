e6d2921427b424bd8c3812dc66ab22ccd6b6ace6
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-06 17:55:01 -0300

    criando rotas

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const server = express()
 
+server.get('/', (req, res) => {
+    return res.send('Hi!')
+})
+
 server.listen(5000, () => {
     console.log('server is running')
 })
