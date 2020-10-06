7db6c727a267b02b92bba6fd985bfdc828b8ba2f
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-06 18:55:35 -0300

    atualizando a página sobre

--- server.js
+++ server.js
@@ -9,15 +9,28 @@ server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', (req, res) => {
-    return res.render('about')
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        name: "Mayk Brito",
+        role: "Instrutor - Rocketseat",
+        description: 'Programador full-stack, focando em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            { name: "Github", url: "https://github.com/maykbrito/" },
+            { name: "Twitter", url: "https://twitter.com/maykbrito/" },
+            { name: "Linkedin", url: "https://linkedin.com/in/maykbrito/" }
+        ]
+    }
+
+    return res.render('about', { about })
 })
 
 server.get('/portfolio', (req, res) => {
-    return res.render('portfolio')
+    return res.render('portfolio', { items: videos })
 })
 
 server.listen(5000, () => {
