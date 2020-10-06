e02a28583d45df19752517a3d16c9b392613a387
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-06 18:29:02 -0300

    exportando e importando javascript

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+]
